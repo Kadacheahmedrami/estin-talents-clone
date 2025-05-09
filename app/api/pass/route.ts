5f39a0b3930d24138d22aed197f7ca133aab4ef9
Kadacheahmedrami <a_kadache@estin.dz>
2025-05-09 12:44:30 +0200

    lol route fixed

--- app/api/pass/route.ts
+++ app/api/pass/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export async function GET() {
-    return NextResponse.json({ message: "ta7chatlk" });
+export async function GET(request: NextRequest) {
+    return NextResponse.json({ message: "ta7chatlk" }, { status: 200 });
 }
